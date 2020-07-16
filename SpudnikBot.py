050388ed2b78fa5394c8f3d7268b6f5d5c01596a
Griffin Clark <32806921+GriffClark@users.noreply.github.com>
2020-07-15 19:41:03 -0700

    improving screenshot functionality

--- SpudnikBot.py
+++ SpudnikBot.py
@@ -12,11 +12,21 @@ from selenium.webdriver.common.keys import Keys
 startTime = datetime.now()
 print(str(startTime))
 
-"""
-TODO
-Have bot be able to click links on eCommerce sites
-Have bot be able to screenshot all ads it sees
-"""
+# TODO steps to problem definition:
+# 1. take screenshots of ads
+# 2. crop screenshots
+# 3. scrape ads for category/content
+# 4. build dashboard to see what ads are being seen relative to time and location
+# 5. create more natural web presence
+# deal with pop-up windows
+# 6. create more natural social media presense
+# 7. create bot that interacts with a browser exactly like a person would - with a mouse that moves around and clicks on elements
+# 8. figure out how to create multiple google accounts
+# 9. let the beasts loose upon the internet, and make sure they're getting ads for laptops
+# 10. figure out how to buy laptops without spending money, in bulk
+# 11. see if ads change after purchase
+
+
 # # INTERNET EXPLORER
 # PATH = "C:\Program Files (x86)\IEDriverServer.exe"
 # driver = webdriver.Ie(PATH) #hoping that using IE will evade bot detection because... you know... its IE
@@ -53,6 +63,31 @@ class Stack:
         return len(self.items)
 
 
+class AdWebsite:
+    def __init__(self, url):
+        self.url = url
+
+    # Love Python variable declarations...
+    xpath = ""
+    y_location = ""
+    id = ""
+
+    def intelligent_screenshot_ad(self):
+        # TODO finish method. Ads might be able to be identified by capturing web traffic with Wireshark and loading
+        #  just the ad URL
+        driver.get(self.url)
+        wait_for_webpage_to_load()
+        if len(self.xpath) > 0:
+            # this means an xpath was set
+            driver.executeScript("arguments[0].scrollIntoView(true);", self.xpath)
+            print("pretend like we took a screenshot")
+
+    def dumb_screenshot_ad(self):
+        driver.get(self.url)
+        wait_for_webpage_to_load()
+        take_screenshot(self.url)
+
+
 def smooth_scroll():
     # this method is designed to confuse any detection software looking for continuous scroll patterns
     default_scroll_distance = 800
@@ -65,9 +100,9 @@ def smooth_scroll():
                                      int(location_history.peek()) + default_scroll_distance)  # this
     # is where we want to end up
 
-    i = 0
+    counter = 0
     steps = 150
-    while i < steps:  # don't care if we get stuck here or not
+    while counter < steps:  # don't care if we get stuck here or not
         time_sleep_max = 115
         time_sleep_now = random.randint(int(time_sleep_max / 2), time_sleep_max)
         time.sleep(time_sleep_now / 1000)
@@ -78,7 +113,7 @@ def smooth_scroll():
                 "window.scrollBy(" + str(location_history.pop()) + "," + str(target_location / steps) + ")", "")
             location_history.push(get_y_offset())
             # print(str(location_history.peek()) + "px" + "/" + str(get_document_height()) + "px " + str(datetime.now()))
-        i += 1
+        counter += 1
         if check_if_stuck():
             break;
 
@@ -96,7 +131,7 @@ def send_modulated_input(data, path, selectorType):
                     driver.find_element_by_xpath(path).send_keys(x)
                 elif selectorType == "id":
                     driver.find_element_by_id(path).send_keys(x)
-                time.sleep((random.randint(65, 85)) / 1000)
+                time.sleep((random.randint(60, 175)) / 1000)
             time.sleep(3)
             break
         except:
@@ -109,8 +144,25 @@ def wait_for_webpage_to_load():
     time.sleep(8)
 
 
+def log_into_amazon():
+    # every login attempt gets flagged as suspicious activity, so we're going to have to get creative...
+    # this has to be run on a box with a custom chrome profile
+    driver.get("https://www.amazon.com")
+    wait_for_webpage_to_load()
+    driver.find_element_by_xpath("/html/body/div[1]/header/div/div[1]/div[2]/div/a[2]/div/span").click()
+    wait_for_webpage_to_load()
+    send_modulated_input(default_email, "//*[@id=\"ap_email\"]", "xpath")
+    driver.find_element_by_xpath("//*[@id=\"ap_email\"]").send_keys(Keys.ENTER)
+    wait_for_webpage_to_load()
+    send_modulated_input(default_password, "//*[@id=\"ap_password\"]", "xpath")
+    driver.find_element_by_xpath("//*[@id=\"ap_password\"]").send_keys(Keys.ENTER)
+    wait_for_webpage_to_load()
+    print("logged into amazon")
+
+
 def log_into_google():
     driver.get("https://www.google.com/")
+    wait_for_webpage_to_load()
     driver.find_element_by_xpath("//*[@id=\"gb_70\"]").click()
     send_modulated_input(default_email, "//*[@id=\"identifierId\"]", "xpath")
     driver.find_element_by_xpath("//*[@id=\"identifierId\"]").send_keys(Keys.ENTER)
@@ -229,21 +281,26 @@ def get_document_height():
 default_file_location = "C:\\Users\\gclar\\PycharmProjects\\Spudnik\\Screenshots\\"  # TODO this is a bad
 
 
-def take_screenshot(webURL):
-    # FIXME finish
-    # TODO put time stamp in filename
-    # TODO figure out why + str(get_y_offset()) isn't working. Maybe it is...
-    split_url = webURL.split('/')
+def build_ad_websites():
+    # manually compiled list of personalized ads and where to find them
+    global ad_website_list
+    cafe_mom = AdWebsite("https://thestir.cafemom.com/parenting_news/226166/target-black-owned-badge")
+    cafe_mom.xpath = "//*[@id=\"adContainer\"]"
+    ad_website_list.append(cafe_mom)
+
+
+def take_screenshot(current_web_url):
+    split_url = current_web_url.split('/')
     raw_datetime_array = str(datetime.now()).split()
     raw_datetime = raw_datetime_array[1] + "_" + raw_datetime_array[0]
     sanitized_datetime_array = raw_datetime.split(':')
     sanitized_seconds = sanitized_datetime_array[2].split('.')[0]
-    sanitized_datetime = sanitized_datetime_array[0]  + sanitized_datetime_array[1] + sanitized_seconds
+    sanitized_datetime = sanitized_datetime_array[0] + sanitized_datetime_array[1] + sanitized_seconds
 
     # files are named *website@hhmmss.png*
-    sanitized_url = split_url[2] + "@" + str(sanitized_datetime) # this line ultimately dictates file naming format
+    sanitized_url = split_url[2] + "@" + str(sanitized_datetime)  # this line ultimately dictates file naming format
 
-    print("took screenshot " + str(sanitized_url) + ".png")
+    print("screenshot saved to " + str(sanitized_url) + ".png")
     driver.save_screenshot(default_file_location + str(sanitized_url) + ".png")
 
 
@@ -280,6 +337,8 @@ social_media_list = [
     "https://www.instagram.com/"
 ]
 
+ad_website_list = []
+
 location_history = Stack()
 location_history.push(0)
 logged_into_reddit = False
@@ -290,7 +349,9 @@ default_email = "isaacMcCafferty081081@gmail.com"
 default_username = "isaacMcCafferty081"
 
 # set up the bot
+build_ad_websites()
 log_into_google()
+# log_into_amazon() # this needs its own machine to work. Amazon's good
 log_into_facebook_and_instagram()
 log_into_reddit()
 
@@ -300,55 +361,56 @@ def get_elapsed_time():
 
 
 last_visit_time = datetime.now()
-
 while True:
+    open_new_window()  # need to open a webpage to get started
     # see if we should go to Reddit to view an ad, or if we should go to the interwebs to keep building our user profile
     print("T+: " + str(get_elapsed_time().seconds) + "s")
-
-    if (datetime.now() - last_visit_time).seconds <= 300:
+    if num_webpages_visited > 0 and num_webpages_visited % 7 == 0:
         scroll_through_social_media(random.randint(10, 50))
         last_visit_time = datetime.now()
-    num_webpages_visited += 1
-    num_scrolls = 0  # resets how many scrolls have been taken on this page
-    num_scrolls_to_complete = random.randint(50, 500)  # maximum number of scrolls until we get bored and leave the
-    # page, regardless of where we are
-    scroll_range_max = get_document_height() / 8  # whats the furthest you can move in one scroll
-    # then hang out for a sec
-    if num_scrolls > 0 and num_scrolls % 7 == 0:
-        print("opening new webpage")
-        open_new_window()
-    elif get_y_offset() >= get_document_height():
-        # if you're most of the way through a document that can be far enough
-        print("hit the bottom. Restarting this whole kerfluffle again... *sigh*")
-        break
     else:
-        smooth_scroll()
-        print("scrolled down a bit " + str(datetime.now()))
-        if check_if_stuck():
-            # if we're stuck, break out of the loop and restart
-            print("Stuck?: " + str(check_if_stuck()))
+        num_scrolls_to_complete = random.randint(50, 500)  # maximum number of scrolls until we get bored and leave the
+        if int(random.randint(0, 9)) == 4:  # 10% chance of opening new webpage
+            print("opening new webpage")
+            open_new_window()
+        elif get_y_offset() >= get_document_height():
+            # if you're most of the way through a document that can be far enough
+            print("hit the bottom. Restarting this whole kerfluffle again... *sigh*")
             break
-        num_scrolls += 1
-        # at this point, we have made a scroll action
-        # Now, see if we should click a link on this webpage
-        all_links = links = driver.find_elements_by_partial_link_text(
-            '')  # many of the things this finds won't be clickable, hence the try/catch block. This is intentional,
-        # as it can be used as a way to add randomness to how often we click
-        l = links[(random.randint(0, len(links) - 1))]  # this is the link we will try to click
-        try:
-            l.click()
-            time.sleep(3)  # allows time for the webpage to load
-            driver.switch_to.window(driver.window_handles[random.randint(len(driver.window_handles) - 1)])
-            if len(driver.window_handles) > 7:
-                driver.close()
-        except:
-            print("error clicking element " + str(l))
+        else:
+            i = 0
+            while i < num_scrolls_to_complete:
+                smooth_scroll()
+                print("scrolled down a bit " + str(datetime.now()))
+                if check_if_stuck():
+                    # if we're stuck, break out of the loop and restart
+                    print("Stuck?: " + str(check_if_stuck()))
+                    break
+                i += 1
+    # after every iteration, see where we're at with ads
+    for element in ad_website_list:
+        element.dumb_screenshot_ad()
+    # at this point, we have made a scroll action
+    # Now, see if we should click a link on this webpage
+    all_links = links = driver.find_elements_by_partial_link_text(
+        '')  # many of the things this finds won't be clickable, hence the try/catch block. This is intentional,
+    # as it can be used as a way to add randomness to how often we click
+    l = links[(random.randint(0, len(links) - 1))]  # this is the link we will try to click
+    try:
+        l.click()
+        time.sleep(3)  # allows time for the webpage to load
+        driver.switch_to.window(driver.window_handles[random.randint(0, len(driver.window_handles) - 1)])
+        if len(driver.window_handles) > 7:
+            driver.close()
+    except:
+        print("error clicking element " + str(l.tag_name))
 
-        # sometimes it should switch between tabs
-        if num_scrolls % int((random.randint(1, 2)) * 1000) == 0:
-            # it may be beneficial to have this hit more often, but not sure how much it affects anything
-            num_open_windows = len(driver.window_handles)
-            try:
-                driver.switch_to.window(driver.window_handles[random.randint(num_open_windows)])
-            except:
-                print("window switcheroo failed")
+    # sometimes it should switch between tabs TODO this isn't working
+    if int(random.randint(0,
+                          9)) == 4:  # TODO turn this and the other identical line into chance_to_perform_operation(probability)
+        # it may be beneficial to have this hit more often, but not sure how much it affects anything
+        num_open_windows = len(driver.window_handles)
+        try:
+            driver.switch_to.window(driver.window_handles[random.randint(num_open_windows)])
+        except:
+            print("window switcheroo failed")
